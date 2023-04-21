7766608f25c32cf3bb43690fd6e2de661bef7fa2
Jihad El Sayed <david.pales1@gmail.com>
2023-04-21 20:00:23 +0200

    fdf

---
+++ main.py
@@ -0,0 +1,41 @@
+import fastapi as _fastapi
+import chain as _chain
+
+chain = _chain.Chain()
+app = _fastapi.FastAPI()
+
+
+# endpoint to mine a block
+@app.post("/mine_block/")
+def mine_block(data: str):
+    if not chain.is_chain_valid():
+        return _fastapi.HTTPException(status_code=400, detail="The chain is invalid")
+    block = chain.mine_block(data=data)
+
+    return block
+
+
+# endpoint to return the chain
+@app.get("/chain/")
+def get_chain():
+    if not chain.is_chain_valid():
+        return _fastapi.HTTPException(status_code=400, detail="The chain is invalid")
+    chain = chain.chain
+    return chain
+
+# endpoint to see if the chain is valid
+@app.get("/validate/")
+def is_chain_valid():
+    if not chain.is_chain_valid():
+        return _fastapi.HTTPException(status_code=400, detail="The chain is invalid")
+
+    return chain.is_chain_valid()
+
+
+# endpoint to return the last block
+@app.get("/chain/last/")
+def previous_block():
+    if not chain.is_chain_valid():
+        return _fastapi.HTTPException(status_code=400, detail="The chain is invalid")
+        
+    return chain.get_previous_block()
